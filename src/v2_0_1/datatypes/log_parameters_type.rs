a674fd2a06b5b2745dd13347394ea0f4c3bcd522
Wolfy7 <andre.decher@gmail.com>
2025-09-03 12:22:00 +0200

    feat: integrate datetime_rfc3339 helper for consistent DateTime serialization across multiple types

--- src/v2_0_1/datatypes/log_parameters_type.rs
+++ src/v2_0_1/datatypes/log_parameters_type.rs
@@ -1,3 +1,4 @@
+use crate::v2_0_1::helpers::datetime_rfc3339;
 use chrono::DateTime;
 use chrono::Utc;
 
@@ -7,8 +8,14 @@ use chrono::Utc;
 #[serde(rename_all = "camelCase")]
 pub struct LogParametersType {
     pub remote_location: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        with = "datetime_rfc3339::option"
+    )]
     pub oldest_timestamp: Option<DateTime<Utc>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        with = "datetime_rfc3339::option"
+    )]
     pub latest_timestamp: Option<DateTime<Utc>>,
 }
--- src/v2_0_1/helpers/datetime_rfc3339.rs
+++ src/v2_0_1/helpers/datetime_rfc3339.rs
@@ -16,3 +16,31 @@ where
         .map(|dt| dt.with_timezone(&Utc))
         .map_err(serde::de::Error::custom)
 }
+
+pub mod option {
+    use chrono::{DateTime, SecondsFormat, Utc};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(date: &Option<DateTime<Utc>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match date {
+            Some(dt) => serializer.serialize_str(&dt.to_rfc3339_opts(SecondsFormat::Millis, true)),
+            None => serializer.serialize_none(),
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let opt = Option::<String>::deserialize(deserializer)?;
+        match opt {
+            Some(s) => DateTime::parse_from_rfc3339(&s)
+                .map(|dt| Some(dt.with_timezone(&Utc)))
+                .map_err(serde::de::Error::custom),
+            None => Ok(None),
+        }
+    }
+}
